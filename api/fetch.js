4433a3f7c37b88f12f5b9e0e05f61c8110d101f3
Shaluka Gimhan <201992093+sh4lu-z@users.noreply.github.com>
2026-06-19 01:04:10 +0530

    Update fetch.js

--- api/fetch.js
+++ api/fetch.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
     const REPO_OWNER = process.env.REPO_OWNER;
     const REPO_NAME = process.env.REPO_NAME;
     const SHARED_SECRET = process.env.SHARED_SECRET;
-    const branch = "cipher-MD-web";
+    const branch = "main";
 
     // 🛡️ OTP පරීක්ෂාව
     const isValid = speakeasy.totp.verify({
